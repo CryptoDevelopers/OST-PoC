6e30fb6058b9c117a926f7660fbed2c316c7d637
Patrick <Blockchainbuddha@gmail.com>
2018-04-26 21:45:39 -0400

    day 1 complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,45 @@
-// app.js
-const express = require('express')
-const app = express()
+App = {
 
-app.get('/', (req, res) => res.send('Hello World!'))
+  init: function(){
+    console.log("App.init()");
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+    var json = JSON.parse('./API.txt')
+
+    const API_KEY = "XXXX";
+    const API_SECRET = json.secret;
+    const Ostkit = require("ostkit");                             // tell node.js that you need ostkit.js
+    const ost=new Ostkit(API_KEY, API_SECRET);      // initialize the API connection
+
+    return App.initWeb3();
+  },
+
+  initDataLayer: function(){
+    //List users/API.txt
+    ost.usersList().then((res) => {
+      console.log(res)
+    });
+
+    console.log("web3 instance initialized");
+    return App.initContract();
+  },
+
+  initContract: function () {
+    $.getJSON("EthSplitter.json", function(artifactData){
+      App.contracts.EthSplitter = TruffleContract(artifactData);
+      App.contracts.EthSplitter.setProvider(App.web3Provider);
+      console.log("initialized EthSplitter contract");
+
+      return App.initUI();
+    });
+  },
+
+  initUI: function () {
+    console.log("App.initUI()");
+    var ethSplitterInstance;
+
+  }
+};
+
+$(window).on("load", function(){
+  App.init();
+});
