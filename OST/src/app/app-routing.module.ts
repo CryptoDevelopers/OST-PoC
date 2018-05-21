2a57cdc06b44f164c643d05abb616d20219069fe
Ayrton Trujillo <ayrtonx123@gmail.com>
2018-05-20 20:36:02 -0400

    Added login/register components

--- OST/src/app/app-routing.module.ts
+++ OST/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JobListComponent }     from './job-list/job-list.component'
 import { JobDetailComponent }   from './job-detail/job-detail.component'
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'job/:jobs_id', component: JobDetailComponent},
-  { path: 'dashboard', component: JobListComponent}
+  { path: 'dashboard', component: JobListComponent},
+  { path: 'login', component:LoginComponent},
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
